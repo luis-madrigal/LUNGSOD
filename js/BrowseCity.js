9f7368d4554383d2247d3dc216441a400bcf68b0
Luis Madrigal <lmadrigal.111@gmail.com>
2017-03-11 17:47:36 +0800

    recent build

---
+++ js/BrowseCity.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+
+	initTiles(CITY_TILES_TOP, $("#topTilesList"));
+	initTiles(CITY_TILES_BOTTOM, $("#bottomTilesList"));
+
+});
+
+function initTiles(tiles, list) {
+	for(var i = 0; i < tiles.length; i++) {
+		var $tile = $("<div>", {"class": "col-lg-4 cityTile", "style": "background-image: url('" +tiles[i].res+ "');"});
+
+		if(i % 3 != 0) {
+			$tile.css("margin-left", "18px");
+		}
+		if(i < 6) {
+			$tile.css("margin-bottom", "26px");
+		}
+
+		var $city = $("<span>", {"class": "cityName"});
+		var city = (tiles[i].city.indexOf(" ") >= 0)? "":" CITY";
+		$city.append(tiles[i].city.toUpperCase() + city);
+
+		var $province = $("<span>", {"class": "cityProvince"});
+		$province.append("\n"+tiles[i].province.toUpperCase());
+
+		var $tagDiv = $("<div>", {"class": "cityTag"});
+
+		for(var j = 0; j < tiles[i].tags.length; j++) {
+			var $tag = $("<img>", {"style": "cityTagIcon", "src": tiles[i].tags[j].res});
+			$tag.attr("width", "60px");
+			$tag.attr("height", "60px");
+			$tagDiv.append($tag);
+		}
+
+		$tile.append($city);
+		$tile.append($province);
+		$tile.append($tagDiv);
+
+		list.append($tile);
+	}
+}
---
+++ js/db.js
@@ -0,0 +1,145 @@
+var ICONS = {
+	EDUCATION: {
+		name: "Education",
+		res: "res/Icons/Education.png"
+	},
+	EMPLOYMENT: {
+		name: "Employment",
+		res: "res/Icons/Employment.png"
+	},
+	HEALTH: {
+		name: "Health",
+		res: "res/Icons/Health.png"
+	},
+	HOUSING: {
+		name: "Housing",
+		res: "res/Icons/Housing.png"
+	},
+	INFRASTRUCTURE: {
+		name: "Infrastructure",
+		res: "res/Icons/Infrastructure.png"
+	},
+	LEISURE: {
+		name: "Leisure",
+		res: "res/Icons/Leisure.png"
+	},
+	LOCALGOV: {
+		name: "Local Gov",
+		res: "res/Icons/Local Gov.png"
+	},
+	TRANSPORTATION: {
+		name: "Transportation",
+		res: "res/Icons/Transportation.png"
+	},
+}
+
+var CITY_TILES_TOP = [
+	{
+		city: "Santa Rosa",
+		province: "Laguna",
+		score: 7.5,
+		res: "res/City Tiles (15-15)/CityTile_SANTAROSA.jpg",
+		tags: [ICONS.HOUSING, ICONS.EMPLOYMENT]
+	},
+	{
+		city: "Bacolod",
+		province: "Negros Occidental",
+		score: 7.5,
+		res: "res/City Tiles (15-15)/CityTile_BACOLOD.jpg",
+		tags: [ICONS.INFRASTRUCTURE, ICONS.LOCALGOV]
+	},
+	{
+		city: "Cagayan De Oro",
+		province: "Misamis Oriental",
+		score: 7.5,
+		res: "res/City Tiles (15-15)/CityTile_CDO.jpg",
+		tags: [ICONS.INFRASTRUCTURE, ICONS.TRANSPORTATION, ICONS.EMPLOYMENT]
+	},
+	{
+		city: "Iligan",
+		province: "Lanao Del Norte",
+		score: 7.5,
+		res: "res/City Tiles (15-15)/CityTile_ILIGAN.jpg",
+		tags: [ICONS.INFRASTRUCTURE, ICONS.TRANSPORTATION, ICONS.EMPLOYMENT]
+	},
+	{
+		city: "Iloilo",
+		province: "Cebu",
+		score: 7.5,
+		res: "res/City Tiles (15-15)/CityTile_ILOILO.jpg",
+		tags: [ICONS.TRANSPORTATION, ICONS.HOUSING]
+	},
+	{
+		city: "Butuan",
+		province: "Caraga",
+		score: 7.5,
+		res: "res/City Tiles (15-15)/CityTile_BUTUAN.jpg",
+		tags: [ICONS.INFRASTRUCTURE, ICONS.HOUSING, ICONS.HEALTH]
+	},
+	{
+		city: "Tarlac",
+		province: "Tarlac",
+		score: 7.5,
+		res: "res/City Tiles (15-15)/CityTile_TARLAC.jpg",
+		tags: [ICONS.HOUSING, ICONS.EMPLOYMENT]
+	},
+	{
+		city: "Legazpi",
+		province: "Bicol",
+		score: 7.5,
+		res: "res/City Tiles (15-15)/CityTile_LEGAZPI.jpg",
+		tags: [ICONS.INFRASTRUCTURE, ICONS.TRANSPORTATION, ICONS.EMPLOYMENT]
+	},
+	{
+		city: "Dumaguete",
+		province: "Negros Oriental",
+		score: 7.5,
+		res: "res/City Tiles (15-15)/CityTile_DUMAGUETE.jpg",
+		tags: [ICONS.EMPLOYMENT, ICONS.EDUCATION]
+	}
+];
+
+var CITY_TILES_BOTTOM = [
+	{
+		city: "Santa Rosa",
+		province: "Laguna",
+		score: 7.5,
+		res: "res/City Tiles (15-15)/CityTile_SANTAROSA.jpg",
+		tags: [ICONS.HOUSING, ICONS.EMPLOYMENT]
+	},
+	{
+		city: "Bacolod",
+		province: "Negros Occidental",
+		score: 7.5,
+		res: "res/City Tiles (15-15)/CityTile_BACOLOD.jpg",
+		tags: [ICONS.INFRASTRUCTURE, ICONS.LOCALGOV]
+	},
+	{
+		city: "Cagayan De Oro",
+		province: "Misamis Oriental",
+		score: 7.5,
+		res: "res/City Tiles (15-15)/CityTile_CDO.jpg",
+		tags: [ICONS.INFRASTRUCTURE, ICONS.TRANSPORTATION, ICONS.EMPLOYMENT]
+	},
+	{
+		city: "Iligan",
+		province: "Lanao Del Norte",
+		score: 7.5,
+		res: "res/City Tiles (15-15)/CityTile_ILIGAN.jpg",
+		tags: [ICONS.INFRASTRUCTURE, ICONS.TRANSPORTATION, ICONS.EMPLOYMENT]
+	},
+	{
+		city: "Iloilo",
+		province: "Cebu",
+		score: 7.5,
+		res: "res/City Tiles (15-15)/CityTile_ILOILO.jpg",
+		tags: [ICONS.TRANSPORTATION, ICONS.HOUSING]
+	},
+	{
+		city: "Butuan",
+		province: "Caraga",
+		score: 7.5,
+		res: "res/City Tiles (15-15)/CityTile_BUTUAN.jpg",
+		tags: [ICONS.INFRASTRUCTURE, ICONS.HOUSING, ICONS.HEALTH]
+	}
+];
