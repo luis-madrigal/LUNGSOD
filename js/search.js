d4a1ab6b70d0dd14707bcbeb9c98099b282a9076
Luis Madrigal <lmadrigal.111@gmail.com>
2017-03-31 12:32:35 +0800

    added autocompletes

---
+++ js/search.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+
+	var names = [];
+	
+	for(var i in CITIES)
+		names.push(CITIES[i].name);
+		
+	
+	$(".tags").autocomplete({
+		source: names,
+		appendTo: $("#bs-example-navbar-collapse-1"),
+		select: function(event, ui){
+			console.log(ui)
+			var value = ui.item.label;
+			var dataId;
+			for(var i in CITIES){
+				console.log(i);
+				if(value == CITIES[i].name){
+					console.log(value);
+					dataId = i;
+					console.log(dataId);
+					break;
+				}
+			}
+			
+			var dest = window.location.href.split("/");
+
+			dest[dest.length-1] = "CityPage.html?city=" + dataId;
+			dest = dest.join("/");
+
+			document.location.href = dest;
+		}
+	});
+
+});
