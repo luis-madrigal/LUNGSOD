5f46f65fdc7244e77ed56af7e2a8b78ab92555b2
Luis Madrigal <lmadrigal.111@gmail.com>
2017-04-19 18:58:07 +0800

    autocomplete done

--- js/search.js
+++ js/search.js
@@ -9,14 +9,14 @@ $(document).ready(function(){
 		source: names,
 		appendTo: $("#bs-example-navbar-collapse-1"),
 		focus: function( event, ui ) {
-			$(this).val( ui.item.name );
+			$(this).val( ui.item.value );
 			return false;
 		},
 		select: function(event, ui){
 			var value = ui.item.name;
 			var dataId;
 
-			$(this).val( value );
+			$(this).val( ui.item.value );
 
 			for(var i in CITIES){
 				if(value == CITIES[i].name){
@@ -39,29 +39,22 @@ $(document).ready(function(){
 		source: names,
 		appendTo: $("#bs-example-navbar-collapse-1"),
 		focus: function( event, ui ) {
-			$(this).val( ui.item.name );
+			$(this).val( ui.item.value );
 			return false;
 		},
 		select: function(event, ui){
-			console.log(ui)
 			var value = ui.item.name;
 			var dataId;
+
+			$(this).val( ui.item.value );
+
 			for(var i in CITIES){
-				console.log(i);
 				if(value == CITIES[i].name){
-					console.log(value);
 					dataId = i;
-					console.log(dataId);
 					break;
 				}
 			}
 			
-			// var dest = window.location.href.split("/");
-
-			// dest[dest.length-1] = "CityPage.html?city=" + dataId;
-			// dest = dest.join("/");
-
-			// document.location.href = dest;
 			compare(dataId)
 			return false;
 		}
@@ -71,13 +64,15 @@ $(document).ready(function(){
 		source: names,
 		appendTo: $("#bs-example-navbar-collapse-1"),
 		focus: function( event, ui ) {
-			$(this).val( ui.item.name );
+			$(this).val( ui.item.value );
 			return false;
 		},
 		select: function(event, ui){
-			$(this).val( ui.item.name );
 			var value = ui.item.name;
 			var dataId;
+
+			$(this).val( ui.item.value );
+
 			for(var i in CITIES){
 				if(value == CITIES[i].name){
 					dataId = i;
@@ -98,7 +93,8 @@ $(document).ready(function(){
 	$.ui.autocomplete.prototype._renderItem = function(ul, item) {
 		return $( "<li></li>" )
 		  .data( "item.autocomplete", item )
-		  .append( "<div>" + item.name + "</div>" )
+		  .append( "<div style = 'color:grey;padding-left:2%;padding-top:1%;'>" + item.value + "</div>" )
 		  .appendTo( ul );
 	}
+
 });
